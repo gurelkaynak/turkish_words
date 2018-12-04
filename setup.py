23e0170ff66cd8caea29acb1c6d6de576f2e060a
Gürel Kaynak <gurelkaynak@gmail.com>
2018-12-04 10:31:11 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='turkish_words',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/gurelkaynak/turkish_words',
+    license='MIT',
+    author='Gurel Kaynak',
+    author_email='gurelkaynak@gmail.com',
+    description='Random turkish words library'
+)
